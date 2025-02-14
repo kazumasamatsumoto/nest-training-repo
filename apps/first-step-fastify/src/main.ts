be6d7bd45cceb51a35f8cf812aed26f862ff4e3a
kazumasamatsumoto <kazumasa.matsumoto62@gmail.com>
2025-02-14 12:35:57 +0900

    fastify

--- apps/first-step-fastify/src/main.ts
+++ apps/first-step-fastify/src/main.ts
@@ -1,14 +1,18 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+// FastifyAdapterをインポート
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // FastifyAdapterを渡してアプリケーションを作成
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
